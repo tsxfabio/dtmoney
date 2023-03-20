40aaed606ad4accf0ab8e7f06c836083c30039d0
Fabio Leardini <leardinifabio90@gmail.com>
2023-03-19 23:44:50 -0300

    tabela de transferências

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,33 @@
 import { DashboardBox } from "../../components/DashboardBox";
-import { HomeContainer } from "./styles";
+import { HomeContainer, PriceHighLight, TableContainer } from "./styles";
 
 export function Home() {
   return (
     <HomeContainer>
       <DashboardBox />
+
+      <TableContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant='income'>R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Hamburguer</td>
+              <td>
+                <PriceHighLight variant='outcome'>-R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/04/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </TableContainer>
     </HomeContainer>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,3 +4,42 @@ export const HomeContainer = styled.div`
   max-width: 1280px;
   margin: 0 auto;
 `;
+
+export const TableContainer = styled.div`
+  width: 100%;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+
+    td {
+      padding: 1.25rem 0.5rem;
+      background-color: ${(props) => props.theme["gray-700"]};
+
+      &:first-child {
+        padding-left: 2rem;
+        border-top-left-radius: 6px;
+        border-bottom-left-radius: 6px;
+      }
+
+      &:last-child {
+        padding-right: 2rem;
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+    }
+  }
+`;
+
+interface PriceHighLightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${(props) =>
+    props.variant === "income"
+      ? props.theme["green-300"]
+      : props.theme["red-300"]};
+`;
