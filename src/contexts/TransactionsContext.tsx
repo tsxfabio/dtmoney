a63cd0256398ef30f97080d953b65057451cabe5
Fabio Leardini <leardinifabio90@gmail.com>
2023-03-26 23:44:13 -0300

    montagem transactions context

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,3 +1,30 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
 
-const TransactionsContext = createContext({});
+/* Inferfaces */
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface Transactions {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  category: string;
+  createAt: string;
+}
+
+interface TransactionsContextType {
+  transections: Transactions[];
+}
+
+const TransactionsContext = createContext({} as TransactionsContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  return (
+    <TransactionsContext.Provider value={{ transections: [] }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
